b6428eb4f3b276f1664fa5f4c52d8186ef011356
Aleksei <lokhin.a.a@gmail.com>
2023-06-06 01:40:08 +0300

    Changed from 06.06.2023 [R, MyRadio-1] - DELETED Storyboard - ADDED Readme

--- MyRadio/ViewController.swift
+++ MyRadio/ViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - Life Cycles Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
     }
-
-
 }
-
